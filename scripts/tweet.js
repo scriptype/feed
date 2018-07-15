5945172a8db7be53c4b474820a2acae3a118e1c2
scriptype <scriptyper@gmail.com>
2018-07-15 05:53:28 +0300

    Implement tweeting and add sample link

--- scripts/tweet.js
+++ scripts/tweet.js
@@ -7,6 +7,29 @@ const client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 })
 
+const tweetTemplate = data => (
+  `I've just read: "${data.title}": ${data.url}`
+)
+
+const tweet = data => new Promise((resolve, reject) => {
+  const endpoint = 'statuses/update'
+  const params = {
+    status: tweetTemplate(data)
+  }
+  client.post(endpoint, params, (err, tweet, response) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+  })
+})
+
+const wait = timeout => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve()
+  }, timeout)
+})
+
 let tweetItems = ''
 
 process.stdin.setEncoding('utf8')
@@ -19,12 +42,11 @@ process.stdin.on('readable', () => {
 })
 
 process.stdin.on('end', () => {
-  console.log('items', JSON.parse(tweetItems))
-})
+  tweetItems = JSON.parse(tweetItems)
 
-client.get('statuses/user_timeline', {}, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets)
-  }
-  console.log('response', response, tweets)
+  tweetItems.reduce((items, item) =>
+    Promise.all(items)
+      .then(() => wait(5000))
+      .then(() => tweet(item))
+  ), [])
 })
