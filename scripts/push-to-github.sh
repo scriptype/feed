1efe1afdc49cd254890e38a4552ec92be3aeb7be
scriptype <scriptyper@gmail.com>
2018-07-16 22:24:54 +0300

    Update github-push script

--- scripts/push-to-github.sh
+++ scripts/push-to-github.sh
@@ -3,4 +3,4 @@ git config user.email "travis@travis-ci.org"
 git status
 git add -A
 git commit -m "Generate data from new list.$LINKS_ADDED"
-git push --quiet "https://${GITHUB_TOKEN}@github.com/scriptype/feed.git" origin HEAD > /dev/null 2>&1
+git push --quiet "https://${GITHUB_TOKEN}@github.com/scriptype/feed.git" origin master > /dev/null 2>&1
