6ed5a07936f6887bfec30e756598d7b6bcf84d5a
scriptype <scriptyper@gmail.com>
2018-07-16 22:09:42 +0300

    Fix github-push script to add all files

--- scripts/push-to-github.sh
+++ scripts/push-to-github.sh
@@ -1,3 +1,4 @@
 git status
-git commit -am "Generate data from new list [by travis].$LINKS_ADDED"
+git add -A
+git commit -m "Generate data from new list [by travis].$LINKS_ADDED"
 git push --quiet "https://${GITHUB_TOKEN}@github.com/scriptype/feed.git" origin HEAD > /dev/null 2>&1
