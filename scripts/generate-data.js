e1e9807979d5cd1a600a410f7e7494911074a742
scriptype <scriptyper@gmail.com>
2018-07-16 23:15:58 +0300

    Fix data folder path

--- scripts/generate-data.js
+++ scripts/generate-data.js
@@ -5,7 +5,7 @@ const { splitByLimit, createFile, cleanFolder } = require('./helpers')
 // Get fresh list of links.
 const links = require('../links')
 
-const dataFolderPath = path.join(__dirname, '..', 'data')
+const dataFolderPath = path.join(__dirname, '..', 'public', 'data')
 
 // Get all of old links, we'll use this to detect newly added links.
 const all = require(path.join(dataFolderPath, 'all'))
