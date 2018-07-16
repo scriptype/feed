fbd203560abd9b5e394a20af6c29769bc776a3da
scriptype <scriptyper@gmail.com>
2018-07-16 22:13:38 +0300

    Add git config to github-push script

--- scripts/push-to-github.sh
+++ scripts/push-to-github.sh
@@ -1,4 +1,6 @@
+git config user.name "Travis CI"
+git config user.email "travis@travis-ci.org"
 git status
 git add -A
-git commit -m "Generate data from new list [by travis].$LINKS_ADDED"
+git commit -m "Generate data from new list.$LINKS_ADDED"
 git push --quiet "https://${GITHUB_TOKEN}@github.com/scriptype/feed.git" origin HEAD > /dev/null 2>&1
