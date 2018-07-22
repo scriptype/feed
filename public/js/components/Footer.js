3381b6b30e552df33df689c4258d09cc98c484ad
scriptype <scriptyper@gmail.com>
2018-07-22 03:07:16 +0300

    Add 'Made by' section to footer

--- public/js/components/Footer.js
+++ public/js/components/Footer.js
@@ -1,8 +1,11 @@
 export default () => `
   <footer class="footer">
     <div class="content-container">
-      <p class="description">
-        Sharing my "Open tabs / Read Later" list, as I go through it. You can follow Reads Feed also on <a target="_blank" href="https://twitter.com/reads_feed">Twitter</a> and <a target="_blank" href="https://readsfeed.tumblr.com">Tumblr</a>, or check the source code at <a target="_blank" href="https://github.com/scriptype/feed">Github</a>.
+      <p>
+        Sharing my "Open tabs / Read Later" list, as I go through it. You can follow Reads Feed on <a target="_blank" href="https://twitter.com/reads_feed">Twitter</a> and <a target="_blank" href="https://readsfeed.tumblr.com">Tumblr</a>, or check the source code at <a target="_blank" href="https://github.com/scriptype/feed">Github</a>.
+      </p>
+      <p>
+        Made by <a href="https://enes.in" target="_blank">Mustafa Enes Ertarhanacı</a>.
       </p>
     </div>
   </footer>
