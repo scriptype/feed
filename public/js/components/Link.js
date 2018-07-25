133994bb5fdbff8c118011190b999434ae07b5c2
scriptype <scriptyper@gmail.com>
2018-07-25 13:04:53 +0300

    Abstract 'truncate' into a generic helper and truncate link title

--- public/js/components/Link.js
+++ public/js/components/Link.js
@@ -1,22 +1,19 @@
-const truncateTitle = (title, charLimit) =>
-  title.length > charLimit
-    ? `${ title.slice(0, charLimit).trim() }...`
-    : title
+import { truncate } from '../lib/utils.js'
 
 const TagsTemplate = (tags) => `
   <ul class="link-tags">
-    ${ tags.map(tag => `
+    ${tags.map(tag => `
       <li class="link-tag">#${tag}</li>
-    `).join('') }
+    `).join('')}
   </ul>
 `
 
 export default (link) => `
   <div class="link-container">
     <a class="link" target="_blank" href="${link.url}">
-      <span class="link-title">${truncateTitle(link.title, 70)}</span>
-      <span class="link-url">${link.url}</span>
-      ${ link.tags.length ? TagsTemplate(link.tags) : '' }
+      <span class="link-title">${truncate(link.title, 70, true)}</span>
+      <span class="link-url">${truncate(link.url, 50, true)}</span>
+      ${link.tags.length ? TagsTemplate(link.tags) : ''}
     </a>
   </div>
 `
--- public/js/lib/utils.js
+++ public/js/lib/utils.js
@@ -1,2 +1,7 @@
 export const isDev = () =>
   new URLSearchParams(document.location.search).has('dev')
+
+export const truncate = (text, limit, ellipsis) =>
+  text.length > limit
+    ? `${text.slice(0, limit).trim()}${ellipsis ? '...' : ''}`
+    : text
