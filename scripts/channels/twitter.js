9f5adfd362f9b538d38e8343f317bc56ef46b850
scriptype <scriptyper@gmail.com>
2023-05-27 03:45:31 +0300

    Twitter channel is simpler now. See #37

--- scripts/channels/twitter.js
+++ scripts/channels/twitter.js
@@ -1,8 +1,6 @@
 const { TwitterApi } = require('twitter-api-v2')
 const Channel = require('../lib/Channel')
 
-const userId = '1018316345849630721'
-
 const client = new TwitterApi({
   appKey: process.env.TWITTER_CONSUMER_KEY,
   appSecret: process.env.TWITTER_CONSUMER_SECRET,
@@ -10,33 +8,16 @@ const client = new TwitterApi({
   accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 })
 
-const statusTemplate = link => (
-  `${link.title}: ${link.url}`
-)
+const statusTemplate = link => {
+  const title = link.isTweet ? link.retweetQuote : link.title
+  return [title, link.url].filter(Boolean).join(': ')
+}
 
 const tweet = item => {
   return client.v2.tweet(statusTemplate(item))
 }
 
-const retweet = item => {
-  return client.v2.retweet(userId, item.tweet.id)
-}
-
-const retweetWithQuote = item => {
-  return client.v2.tweet(item.tweet.quote, {
-    attachment_url: item.tweet.tweetUrl
-  })
-}
-
 module.exports = new Channel({
   name: 'twitter',
-  method(item) {
-    if (item.tweet) {
-      if (item.tweet.quote) {
-        return retweetWithQuote(item)
-      }
-      return retweet(item)
-    }
-    return tweet(item)
-  }
+  method: tweet
 })
