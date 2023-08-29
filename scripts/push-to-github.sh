b9abfe3b46177ac8a9a59d8aa916c5f9f4e25863
scriptype <scriptyper@gmail.com>
2023-08-29 20:52:12 +0300

    Remove forgotten github push script

--- scripts/push-to-github.sh
+++
@@ -1,7 +0,0 @@
-git checkout master
-git config --global user.name "Travis CI"
-git config --global user.email "travis@travis-ci.org"
-git add -A
-git commit -am "[skip ci] Generate data from new list.$LINKS_ADDED"
-git status
-git push --quiet https://${GITHUB_TOKEN}@github.com/scriptype/feed.git > /dev/null 2>&1
