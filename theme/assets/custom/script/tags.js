94b7b7205e074e288e716eadee056e6d6b0787d0
scriptype <scriptyper@gmail.com>
2024-04-06 01:30:35 +0300

    Fix mobile issues with tags-list
    And revert tags-list to be collapsed by default, because otherwise, in mobile the open state flickers until js kicks in. Now it's closed but the toggle label is visible until js-enhanced, preserving accessibility

--- theme/assets/custom/script/tags.js
+++ theme/assets/custom/script/tags.js
@@ -1,27 +1,35 @@
 const init = () => {
-  const listToggle = document.querySelector('.tags-list-toggle')
-  const listToggleBtn = listToggle.querySelector('.tags-list-toggle-label')
+  const listContainer = document.querySelector('.tags-list-container')
+  const listToggleBtn = listContainer.querySelector('.tags-list-toggle-label')
   const toggleLabels = {
     clickToOpen: 'Show topics',
     clickToClose: 'Hide topics'
   }
 
   const listToggleAutoExpandHandler = () => {
-    if (window.innerWidth <= 900 && listToggle.hasAttribute('open')) {
-      listToggle.removeAttribute('open')
+    if (window.innerWidth <= 900 && listContainer.hasAttribute('open')) {
+      listContainer.removeAttribute('open')
       listToggleBtn.textContent = toggleLabels.clickToOpen
     }
     if (window.innerWidth > 900) {
-      listToggle.setAttribute('open', true)
+      listContainer.setAttribute('open', true)
       listToggleBtn.textContent = toggleLabels.clickToClose
     }
   }
 
   listToggleBtn.addEventListener('click', () => {
     if (listToggleBtn.textContent === toggleLabels.clickToClose) {
-      listToggleBtn.textContent = toggleLabels.clickToOpen
+      // Changing toggle text immediately somehow causes mobile safari
+      // not to expand the details element at all.
+      setTimeout(() => {
+        listToggleBtn.textContent = toggleLabels.clickToOpen
+        document.body.classList.remove('tags-list-is-open')
+      }, 200)
     } else {
-      listToggleBtn.textContent = toggleLabels.clickToClose
+      setTimeout(() => {
+        listToggleBtn.textContent = toggleLabels.clickToClose
+        document.body.classList.add('tags-list-is-open')
+      }, 200)
     }
   })
 
